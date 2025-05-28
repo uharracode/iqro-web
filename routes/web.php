ec5ca40518d79ee724f150c362e3d0e24a28068e
ahmad <ahmadsija2023@gmail.com>
2025-05-28 10:41:29 +0700

    tampilan view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Htpp\Controllers\HomePageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomePageController::class, 'index'])->name('home.index');
